020b58dc6cf49d88813193753a6e30c9ad0ffbf3
Ana Lee <orange665321@gmail.com>
2024-05-02 12:30:32 +0300

    try_2

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,166 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Select } from '../select';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import React, { useState, useRef, useEffect } from 'react';
+import {
+	OptionType,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { Text } from '../text';
+
+interface ArticleParamsFormProps {
+	articleState: ArticleStateType;
+	applyChanges: (newState: ArticleStateType) => void;
+}
+
+export const ArticleParamsForm: React.FC<ArticleParamsFormProps> = ({
+	articleState,
+	applyChanges,
+}) => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [fontFamily, setFontFamily] = useState(articleState.fontFamilyOption);
+	const [fontColor, setFontColor] = useState(articleState.fontColor);
+	const [fontSize, setFontSize] = useState(articleState.fontSizeOption);
+	const [backgroundColor, setBackgroundColor] = useState(
+		articleState.backgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(articleState.contentWidth);
+
+	const sidebarRef = useRef<HTMLDivElement>(null);
+
+	const handleButtonArrowClick = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
+	const handleFontChange = (selectedOption: OptionType) => {
+		setFontFamily(selectedOption);
+	};
+
+	const handleFontSizeChange = (selectedOption: OptionType) => {
+		setFontSize(selectedOption);
+	};
+
+	const handleFontColorChange = (selectedOption: OptionType) => {
+		setFontColor(selectedOption);
+	};
+
+	const handleBackgroundColorChange = (selectedOption: OptionType) => {
+		setBackgroundColor(selectedOption);
+	};
+
+	const handleContentWidthChange = (selectedOption: OptionType) => {
+		setContentWidth(selectedOption);
+	};
+
+	const handleFormReset = () => {
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontColor(defaultArticleState.fontColor);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+	};
+
+	const handleApplyChanges = () => {
+		const newState = {
+			fontFamilyOption: fontFamily,
+			fontColor: fontColor,
+			fontSizeOption: fontSize,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+		};
+		applyChanges(newState);
+	};
+
+	useEffect(() => {
+		const handleClickOverlay = (event: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				setIsMenuOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOverlay);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOverlay);
+		};
+	}, [isMenuOpen]);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isMenuOpen} onClick={handleButtonArrowClick} />
+			<aside
+				ref={sidebarRef}
+				className={`${styles.container}
+				${isMenuOpen ? styles.container_open : ''}`}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => e.preventDefault()}
+				 >
+					<Text
+						size={31}
+						weight={800}
+						fontStyle='normal'
+						uppercase
+						align='left'>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={fontFamily}
+						onChange={handleFontChange}
+						options={fontFamilyOptions}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='font-size'
+						selected={fontSize}
+						onChange={handleFontSizeChange}
+						options={fontSizeOptions}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={fontColor}
+						onChange={handleFontColorChange}
+						options={fontColors}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={backgroundColor}
+						onChange={handleBackgroundColorChange}
+						options={backgroundColors}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={contentWidth}
+						options={contentWidthArr}
+						onChange={handleContentWidthChange}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title='Сбросить' type='reset' onClick={handleFormReset} />
+						<Button
+							title='Применить'
+							type='submit'
+							onClick={handleApplyChanges}
+//Обработчик сабмита нужно навешивать только на тег form с событием submit,
+//а не на кнопку сабмита с событием click,
+//так как сабмит формы происходит ещё при нажатии Enter,
+//и он не будет работать, если навесить обработчик клика на кнопку только.
+
+						/>
 					</div>
 				</form>
 			</aside>
